455044a05ba564a66939094e6e7ed676cba0a700
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-08-28 23:37:06 +0300

    Task 2 : Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,15 @@ import uuid
 from typing import Union
 
 
+def count_calls(method):
+    """ Count the number of times a method is called from the cache """
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
 class Cache:
     """ My cache class """
 
@@ -19,7 +28,7 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
+
     def get(self, key: str, fn: callable = None):
         """ Getting the data from redis """
         data = self._redis.get(key)
@@ -28,11 +37,11 @@ class Cache:
         if fn:
             return fn(data)
         return data
-    
+
     def get_str(self, key: str) -> str:
         """ Getting the data from redis as string """
         return self.get(key, lambda d: d.decode("utf-8"))
-    
+
     def get_int(self, key: str) -> int:
         """ Getting the data from redis as int """
         return self.get(key, int)
